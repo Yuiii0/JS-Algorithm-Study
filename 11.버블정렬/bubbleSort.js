8916f70ad60cb117b00fe7f8dab2fcd6011e2d63
Yujiii0 <yuji918@naver.com>
2024-03-29 16:10:05 +0900

    feat:bubbleSort.js

---
+++ 11.버블정렬/bubbleSort.js
@@ -0,0 +1,18 @@
+import { swap } from "./swap";
+
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      // console.log(arr, arr[j], arr[i]);
+      if (arr[j] > arr[j + 1]) {
+        //swap!
+        swap(arr, j, j + 1);
+
+        // let temp = arr[j];
+        // arr[j] = arr[j + 1];
+        // arr[j + 1] = arr[j];
+      }
+    }
+  }
+  return arr;
+}
